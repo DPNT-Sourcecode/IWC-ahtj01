2cc281890b9c3cd0de63d1a154aa9a08f2ec9ad8
sbx_user1051 <sbx_user1051@169.254.14.213>
2026-03-06 15:18:19 +0000

    Fri Mar 06 15:18:19 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -10,10 +10,19 @@ def test_enqueue_size_invalid_timestamp() -> None:
     ])
 
 
+def test_enqueue_size_string_timestamp() -> None:
+    run_queue([
+        call_enqueue("companies_house", 1, "").expect(1),
+        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=0)).expect(2),
+        call_size().expect(2),
+        call_dequeue().expect("companies_house", 1),
+        call_dequeue().expect("bank_statements", 1),
+    ])
+
 def test_enqueue_size_invalid_provider() -> None:
     # code does actually allow the provider to be added, even though it guards for the existence of the provider
     run_queue([
-        call_enqueue("", 1,  iso_ts(delta_minutes=0)).expect(1),
+        call_enqueue("", 1, iso_ts(delta_minutes=0)).expect(1),
         call_size().expect(1),
     ])
 
@@ -134,5 +143,6 @@ def test_2_high_priority_groups_order_by_earliest_timestamp() -> None:
         call_dequeue().expect("id_verification", 1),
         call_dequeue().expect("bank_statements", 1),
     ])
+
 
 
