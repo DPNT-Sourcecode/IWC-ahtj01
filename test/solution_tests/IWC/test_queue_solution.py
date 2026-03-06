019f6491d81916a0c838bfc5748911e2b47417ed
sbx_user1051 <sbx_user1051@169.254.14.213>
2026-03-06 14:38:06 +0000

    Fri Mar 06 14:38:06 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -57,24 +57,21 @@ def test_tasks_order_by_timestamp() -> None:
 def test_tasks_with_duplicate_order_by_timestamp() -> None:
     run_queue([
         call_enqueue("bank_statements", 1, iso_ts(delta_minutes=5)).expect(1),
-        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=5)).expect(2),
-        call_enqueue("bank_statements", 2, iso_ts(delta_minutes=0)).expect(3),
-        call_size().expect(3),
+        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=5)).expect(1),
+        call_enqueue("bank_statements", 2, iso_ts(delta_minutes=0)).expect(2),
+        call_size().expect(2),
         call_dequeue().expect("bank_statements", 2),
         call_dequeue().expect("bank_statements", 1),
-        call_dequeue().expect("bank_statements", 1),
     ])
-
 
 def test_tasks_with_duplicate_provider_different_timestamp_order_by_timestamp() -> None:
     run_queue([
         call_enqueue("bank_statements", 1, iso_ts(delta_minutes=0)).expect(1),
-        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=5)).expect(2),
-        call_enqueue("bank_statements", 2, iso_ts(delta_minutes=0)).expect(3),
-        call_size().expect(3),
+        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=5)).expect(1),
+        call_enqueue("bank_statements", 2, iso_ts(delta_minutes=0)).expect(2),
+        call_size().expect(2),
         call_dequeue().expect("bank_statements", 1),
         call_dequeue().expect("bank_statements", 2),
-        call_dequeue().expect("bank_statements", 1),
     ])
 
 def test_tasks_order_by_earliest_timestamp() -> None:
@@ -121,5 +118,6 @@ def test_2_high_priority_groups_order_by_earliest_timestamp() -> None:
         call_dequeue().expect("id_verification", 1),
         call_dequeue().expect("bank_statements", 1),
     ])
+
 
 
