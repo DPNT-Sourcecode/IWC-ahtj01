28575640e5e25bf5e140c1471bded9579e7adb91
sbx_user1051 <sbx_user1051@169.254.14.213>
2026-03-06 14:10:10 +0000

    Fri Mar 06 14:10:10 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -55,18 +55,21 @@ def test_tasks_order_by_timestamp() -> None:
     ])
 
 
-def test_2_users_with_3_tasks() -> None:
+def test_2_users_with_3_tasks_orders_by_earliest_timestamp() -> None:
     run_queue([
-        call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(1),
+        call_enqueue("companies_house", 1, iso_ts(delta_minutes=5)).expect(1),
         call_enqueue("bank_statements", 2, iso_ts(delta_minutes=0)).expect(2),
         call_enqueue("id_verification", 1, iso_ts(delta_minutes=0)).expect(3),
         call_enqueue("bank_statements", 1, iso_ts(delta_minutes=0)).expect(4),
-        call_enqueue("id_verification", 2, iso_ts(delta_minutes=0)).expect(3),
-        call_enqueue("companies_house", 2, iso_ts(delta_minutes=0)).expect(4),
-        call_size().expect(4),
-        call_dequeue().expect("companies_house", 1),
-        call_dequeue().expect("id_verification", 1),
-        call_dequeue().expect("bank_statements", 1),
+        call_enqueue("id_verification", 2, iso_ts(delta_minutes=0)).expect(5),
+        call_enqueue("companies_house", 2, iso_ts(delta_minutes=0)).expect(6),
+        call_size().expect(6),
         call_dequeue().expect("bank_statements", 2),
+        call_dequeue().expect("id_verification", 2),
+        call_dequeue().expect("companies_house", 2),
+        call_dequeue().expect("bank_statements", 1),
+        call_dequeue().expect("id_verification", 1),
+        call_dequeue().expect("companies_house", 1),
     ])
+
 
