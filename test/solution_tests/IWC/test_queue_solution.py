4b9137e6ce8526714d2f79e8fa9ea9baf796f800
sbx_user1051 <sbx_user1051@169.254.14.213>
2026-03-06 17:17:55 +0000

    Fri Mar 06 17:17:55 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -250,3 +250,19 @@ def test_limit_deferring_bank_statements_to_5_minutes() -> None:
         call_dequeue().expect("bank_statements", 2),
         call_dequeue().expect("companies_house", 3),
     ])
+
+
+def test_limit_deferring_bank_statements_to_5_minutes_with_clashes() -> None:
+    run_queue([
+        call_enqueue("id_verification", 1, iso_ts(delta_minutes=0)).expect(1),
+        call_enqueue("bank_statements", 2, iso_ts(delta_minutes=2)).expect(2),
+        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=2)).expect(3),
+        call_enqueue("companies_house", 1, iso_ts(delta_minutes=3)).expect(4),
+        call_enqueue("companies_house", 3, iso_ts(delta_minutes=10)).expect(5),
+        call_size().expect(5),
+        call_dequeue().expect("id_verification", 1),
+        call_dequeue().expect("bank_statements", 2),
+        call_dequeue().expect("companies_house", 1),
+        call_dequeue().expect("companies_house", 1),
+        call_dequeue().expect("companies_house", 3),
+    ])
