e6c88d7f1c119ca468f5f38f8f37ac1f7010f6ba
sbx_user1051 <sbx_user1051@169.254.51.197>
2026-03-08 16:23:37 +0000

    Sun Mar 08 16:23:37 UTC 2026

--- lib/solutions/IWC/queue_solution_legacy.py
+++ lib/solutions/IWC/queue_solution_legacy.py
@@ -69,12 +69,16 @@ class Queue:
                 self._update_timestamp_for_existing_task(existing_task=existing_task, new_task=task)
                 continue
 
+            fifo_order = 1
+            if task.provider == BANK_STATEMENTS_PROVIDER.name:
+                total_bank_statement_tasks_with_same_timestamp = sum(1 for t in self._queue if t.provider == BANK_STATEMENTS_PROVIDER.name and t.timestamp == task.timestamp)
+                if total_bank_statement_tasks_with_same_timestamp > 0:
+                    fifo_order = total_bank_statement_tasks_with_same_timestamp
+
             metadata = task.metadata
             metadata.setdefault("priority", Priority.NORMAL)
             metadata.setdefault("group_earliest_timestamp", MAX_TIMESTAMP)
-            metadata.setdefault('fifo_order', 1
-                if task.provider != BANK_STATEMENTS_PROVIDER.name
-                else sum(1 for t in self._queue if t.provider == BANK_STATEMENTS_PROVIDER.name and t.timestamp == task.timestamp))
+            metadata.setdefault('fifo_order', fifo_order)
 
             self._queue.append(task)
         return self.size
@@ -327,3 +331,4 @@ async def queue_worker():
         logger.info(f"Finished task: {task}")
 ```
 """
+
