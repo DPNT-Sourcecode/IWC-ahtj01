ce16e716b07257de3a0952b832a649ad3c6f1fb4
sbx_user1051 <sbx_user1051@169.254.51.197>
2026-03-08 20:01:14 +0000

    Sun Mar 08 20:01:14 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -284,3 +284,16 @@ def test_IWC_R5_S5() -> None:
         call_dequeue().expect("companies_house", 1),
         call_dequeue().expect("id_verification", 6),
     ])
+
+def test_IWC_R5_S6() -> None:
+    run_queue([
+        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=0)).expect(1),
+        call_enqueue("companies_house", 2, iso_ts(delta_minutes=1)).expect(2),
+        call_enqueue("id_verification", 2, iso_ts(delta_minutes=6)).expect(3),
+        call_enqueue("bank_statements", 2, iso_ts(delta_minutes=7)).expect(4),
+        call_size().expect(4),
+        call_dequeue().expect("bank_statements", 1),
+        call_dequeue().expect("companies_house", 2),
+        call_dequeue().expect("id_verification", 2),
+        call_dequeue().expect("bank_statements", 2),
+    ])
